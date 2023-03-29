dd0ae12c35f428fc87dbaab7d3acdd9a7649c367
Katerina Miranda <mirandakaterina1@gmail.com>
2023-03-29 09:48:06 -0300

    contador carrito

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -15,7 +15,7 @@ const CartWidget = () => {
     <>
       <Link className='nav-link' to={'/cart'}>
         <button className='btn btn-dark boton rojo'><IoIosCart size='1.5rem' color='white'/></button>
-        {getItemQuantity() > 0 && <span className='cantCarrito'>{getItemQuantity()}</span>}
+        {getItemQuantity() > 0 && <span className='cantCarrito boton'>{getItemQuantity()}</span>}
       </Link>
     </>
   );
